5c08ef79e2e25de4c0a2edb4a7432a3550032961
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-06-03 21:20:02 -0700

    working on rooms

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,4 @@
-from room import Room
+from room import Room, Rooms
 
 # Declare all the rooms
 
@@ -20,29 +20,61 @@ to north. The smell of gold permeates the air."""),
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
+# r = Room('Master Bedroom', 'Nothing good is ever found here.')
+# print(r)
+# my_rooms = {
+#     'master': Room('Master Bedroom', 'Nothing good is ever found here.'),
+# }
+# print(my_rooms['master'].get_description())
+# print(room['outside'].get_description())
 
+for key in room:
+    print('key')
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
+
+
 
 #
 # Main
 #
-
+#print(room['outside'])
 # Make a new player object that is currently in the 'outside' room.
 
-# Write a loop that:
+import getpass
+
+# playerName = Player(getpass.getuser().title())
 #
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
+# print("Welcome{}".format(playerName.__str__()),"press a directional key ('W,A,S,D') to move...")
+#
+# # Write a loop that:
+#
+# tries = 0
+# while tries < 10:
+#     tries += 1
+#     entry = str(input())
+#     playerName.change_rooms(entry)
+#     # * Prints the current room name
+#     print(f" You are now in the '{playerName.get_current_room()}'")
+#     # * Prints the current description (the textwrap module might be useful here).
+
+
+
+
+
+
+# print(room.__str__())
+
+
 # * Waits for user input and decides what to do.
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
